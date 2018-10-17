c6defad782c22cfa0b217807acd61105a5ada223
Erin Bennett <erindb@stanford.edu>
2018-10-17 09:01:28 -0700

    scriptify skipthoughts

--- run.sh
+++ run.sh
@@ -4,3 +4,8 @@ source activate dod
 jupyter nbconvert --to script read_data.ipynb
 python read_data.py
 source deactivate
+
+source activate skipthoughts
+jupyter nbconvert --to script run_skipthoughts.ipynb
+python run_skipthoughts.py
+source deactivate
