b2010446e539134142f473b03078c2ad6c58a985
Erin Bennett <erindb@stanford.edu>
2018-10-18 23:17:24 -0700

    scriptify with arg for n_documents

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
 #/usr/bin/env bash
 
+n_documents=$1
+
 source activate dod
 jupyter nbconvert --to script read_data.ipynb
-python read_data.py
+python read_data.py --n_documents $n_documents
 source deactivate
 
 source activate skipthoughts
 jupyter nbconvert --to script run_skipthoughts.ipynb
-python run_skipthoughts.py
+python run_skipthoughts.py --n_documents $n_documents
 source deactivate
+
+source activate dod
+jupyter nbconvert --to script distances.ipynb
+python distances.py --n_documents $n_documents
+source deactivate
+
