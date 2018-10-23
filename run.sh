1ece0da046a16d79f6c30d819b8c5350278eac36
Erin Bennett <erindb@stanford.edu>
2018-10-22 22:24:13 -0700

    work on speed

--- run.sh
+++ run.sh
@@ -1,19 +1,25 @@
 #/usr/bin/env bash
 
 n_documents=$1
+cuda=$2
 
 source activate dod
+# read wikitext-2
 jupyter nbconvert --to script read_data.ipynb
 python read_data.py --n_documents $n_documents
+
+# get arora embeddings
+# ...
+
 source deactivate
 
 source activate skipthoughts
 jupyter nbconvert --to script run_skipthoughts.ipynb
-python run_skipthoughts.py --n_documents $n_documents
+THEANO_FLAGS='device=cuda'$cuda python run_skipthoughts.py --n_documents $n_documents
 source deactivate
 
-source activate dod
-jupyter nbconvert --to script distances.ipynb
-python distances.py --n_documents $n_documents
-source deactivate
+# source activate dod
+# jupyter nbconvert --to script distances.ipynb
+# python distances.py --n_documents $n_documents
+# source deactivate
 
